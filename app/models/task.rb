f8b93bfa2e1b59be4678e0efaa7af4500f415cb2
tanaka333 <tanakadiy525@gmail.com>
2018-08-06 03:58:41 +0000

    add column

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
-   validates :content, presence: true, length: { maximum: 10 }
+   validates :content, presence: true, length: { maximum: 200 }
+   validates :status, presence: true, length: {maximum: 10}
 end
